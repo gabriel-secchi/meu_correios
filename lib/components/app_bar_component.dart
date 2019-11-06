bb06caa9ca6b41f57ca8ef3430598d5b43bb977e
Gabriel <gabriel@retta.com.br>
2019-11-06 13:17:52 -0300

    Iniciando projeto

---
+++ lib/components/app_bar_component.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+class AppBarComponent extends StatefulWidget implements PreferredSizeWidget {
+  String textTitle;
+  TabBar bottom;
+  final void Function(String) filerCallback;
+
+  AppBarComponent({
+    Key key, 
+    this.textTitle, 
+    this.bottom,
+    this.filerCallback,
+  }) : super(key: key);
+
+  @override
+  _AppBarComponentState createState() => _AppBarComponentState();
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => setPreferredSize();
+
+  Size setPreferredSize() {
+    return Size(
+      this.bottom.preferredSize.width,
+      this.bottom.preferredSize.height * 2
+    );
+  }
+}
+
+class _AppBarComponentState extends State<AppBarComponent> with SingleTickerProviderStateMixin {
+  Widget widgetTitle;
+  Icon actionIconFilter;
+  
+  int animationStage = 1;
+  AnimationController animationControllerTitle;
+  Animation<double> animationTitle;
+
+    
+  @override
+  void initState() {
+    super.initState();
+    widgetTitle = new Text(widget.textTitle);
+    actionIconFilter = Icon(Icons.search);
+
+    const animBegin = 1.0;
+    const animEnd = 0.0;
+
+    animationControllerTitle = AnimationController(duration: const Duration(milliseconds: 250), vsync: this);
+    animationTitle = Tween<double>(begin: animBegin, end: animEnd).animate(animationControllerTitle)
+      ..addListener((){
+        setState(() {});
+      });
+
+    animationControllerTitle.addStatusListener((status) {
+      setState(() {
+        if(status == AnimationStatus.completed) {
+          if(animationStage == 1) {
+            addFiterField();
+          } 
+          else if(animationStage == 2) {
+            removeFilterField();
+            this.widget.filerCallback(null);
+          }
+        }
+      });
+    });
+  }
+
+  addFiterField(){
+    setState(() {
+      this.widgetTitle = new TextField(
+        style: new TextStyle(
+          color: Colors.white,
+        ),
+        decoration: new InputDecoration(
+          prefixIcon: new Icon(Icons.search,color: Colors.white),
+          hintText: "Search...",
+          hintStyle: new TextStyle(color: Colors.white)
+        ),
+        autofocus: true,
+        //onChanged: (_text) => this.widget.filerCallback,
+        onChanged: (_text) => this.widget.filerCallback(_text),
+      );
+      animationStage = 2;
+      animationControllerTitle.reverse();
+    });
+  }
+
+  removeFilterField() {
+    setState(() {
+      this.widgetTitle = new Text(this.widget.textTitle);
+      animationStage = 1;
+      animationControllerTitle.reverse();
+    });
+  }
+
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationControllerTitle.dispose();
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar( 
+      title: Opacity(
+        opacity: animationTitle.value,
+        child: this.widgetTitle,
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: this.actionIconFilter,
+          onPressed: (){
+            setState(() {
+              if(this.actionIconFilter.icon == Icons.search)
+                this.actionIconFilter = Icon(Icons.close);
+              else 
+                this.actionIconFilter = Icon(Icons.search);
+
+              animationControllerTitle.forward();
+            });
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.refresh),
+          onPressed: () => setState (() {
+            //
+          }),
+        ),
+      ],
+      bottom: widget.bottom,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:meu_correios/components/app_bar_component.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  int _count = 0;
+  
+  onFilterChange(String asd) {
+    setState(() {
+      log(asd);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBarComponent(
+            textTitle: widget.title,
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.all_inclusive)),
+                Tab(icon: Icon(Icons.local_shipping)),
+                Tab(icon: Icon(Icons.archive)),
+              ],
+            ),
+            filerCallback: (_param) => onFilterChange(_param),
+          ),
+          
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Scaffold(
+                body: Center (child: Text("teste gabriel num: $_count"),),
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () => setState(() {
+                    _count++;
+                  }),
+                  tooltip: 'Increment Counter',
+                  child: Icon(Icons.add),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
