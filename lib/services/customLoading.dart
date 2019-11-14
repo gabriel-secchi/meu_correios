e09aece84c7a7830e113376b7cfa52d3a8e4ef8a
gabrielsecchi <gabrielsecchi@hotmail.com>
2019-11-13 23:23:37 -0300

    Add custom loading para aguardar o rastreio dos pacotes

---
+++ lib/services/customLoading.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CustomLoading {
+
+  BuildContext _context;
+
+  CustomLoading(this._context);
+
+  show( onLifeLoading ) async {
+    final loading = Scaffold.of(this._context);
+    loading.showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.black45,
+        content: Center(
+          child: new CircularProgressIndicator(
+            valueColor: new AlwaysStoppedAnimation<Color>(Colors.blue),
+            backgroundColor: Colors.grey
+          )
+        )
+      )
+    );
+
+    final result = await onLifeLoading();
+
+    loading.deactivate();
+
+    return result;
+  }
+
+}
